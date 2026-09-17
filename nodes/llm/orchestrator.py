3372823c460f9f2a72c07d0dd240a1a9589ec103
vikibeksumi <vikibesumi3010@gmail.com>
2026-09-17 15:16:46 +0530

    Add: orchestrator node

--- nodes/llm/orchestrator.py
+++ nodes/llm/orchestrator.py
@@ -1,6 +1,31 @@
 
-from graph.state import InventraState
+from state.state import InventraState
+from prompts.prompts import ORCHESTRATOR_SYSTEM_PROMPT
+from schemas.llm_schemas import OrchestratorOutput
+from langchain_core.messages import SystemMessage
 
-def orchestrator(state: InventraState):
 
-    ...
+class Orchestrator():
+
+    def __init__(self, llm):
+        self.llm_orchestrator = llm.with_structured_output(OrchestratorOutput)
+
+    def call_orchestrator(self, state: InventraState):
+
+        history = state["messages"]
+        messages = [
+            SystemMessage(content=ORCHESTRATOR_SYSTEM_PROMPT),
+            *history
+        ]
+
+        response = self.llm_orchestrator.invoke(input=messages)
+
+        return {
+            "next_destination": response.next_destination,
+            "instruction": response.instruction,
+            "clarification_question": response.clarification_question,
+            "sku": response.sku,
+            "warehouse_id": response.warehouse_id,
+            "target_cover_days": response.target_cover_days,
+            "strategy_hint": response.strategy_hint
+        }
