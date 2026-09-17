0ce6cf0b48342f8e4d98123533e5c56a629af32e
vikibeksumi <vikibesumi3010@gmail.com>
2026-09-18 00:19:01 +0530

    feat(hitl): add clarify_hitl node

---
+++ nodes/hitl/clarify_hitl.py
@@ -0,0 +1,16 @@
+from langgraph.types import interrupt
+from langchain_core.messages import AIMessage, HumanMessage
+from state.state import InventraState
+
+def clarify_hitl(state: InventraState):
+
+    question = state.get("clarification_question")
+    answer = interrupt({"question": question})
+
+    return {
+        "messages": [
+            AIMessage(content=question),
+            HumanMessage(content=answer),
+        ],
+        "clarification_question": None,
+    }
