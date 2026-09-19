35b4a433639970aac2296e4ec2608ae38046584f
vikibeksumi <vikibesumi3010@gmail.com>
2026-09-19 15:23:36 +0530

    feat(router): add roter_approve_hitl for approve_hitl_node

--- workflow/router.py
+++ workflow/router.py
@@ -1,6 +1,6 @@
 from state.state import InventraState
 from langgraph.graph import END
-
+from typing import Optional
 
 
 ORCHESTRATOR_ROUTES = {
@@ -28,12 +28,19 @@ REPLENISHMENT_ROUTES = {
     "blocked": END
 }
 
+APPROVE_HITL_ROUTES = {
+    #decision : nodes
+    "approve": "validate_node",
+    "revise": "replenishment_agent_node",
+    "reject": END
+}
+
 def router_orchestrator(state: InventraState) -> str:
 
     next_destination = state.get("next_destination")
 
     if next_destination not in ORCHESTRATOR_ROUTES:
-        raise ValueError(f"Unknown next_destination: {next_destination}")
+        raise ValueError(f"Unknown next_destination: {next_destination!r}")
 
     return ORCHESTRATOR_ROUTES[next_destination]
 
@@ -51,6 +58,15 @@ def router_replenishment_agent(state: InventraState) -> str:
     replenishment_status = state.get("replenishment_status")
 
     if replenishment_status not in REPLENISHMENT_ROUTES:
-        raise ValueError(f"Unknown inventory_status: {replenishment_status!r}")
+        raise ValueError(f"Unknown replenishment_status: {replenishment_status!r}")
 
     return REPLENISHMENT_ROUTES[replenishment_status]
+
+
+def router_approve_hitl(state: InventraState) -> str:
+    decision = state.get("decision")
+
+    if decision not in APPROVE_HITL_ROUTES:
+        raise ValueError(f"Unknown decision: {decision!r}")
+
+    return APPROVE_HITL_ROUTES[decision]
